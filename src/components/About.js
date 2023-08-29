718fcd11ba1704e71e15adb560a673c190638434
Nisarg6502 <nisarg6502@gmail.com>
2023-08-29 19:14:11 +0530

    About section done

--- src/components/About.js
+++ src/components/About.js
@@ -1,123 +1,74 @@
 import React from 'react';
 import './About.css';
+import Farm from '../images/farm.png';
+import Quality from '../images/quality.png';
+import Handshake from '../images/handshake1.png';
+import Efficiency from '../images/efficiency.png';
 
 export default function About() {
     return (
         <div>
             <section class="text-gray-600 body-font ab">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="text-center mb-20">
-                        <h1 className='title px-5'>SKY HARBOUR</h1>
-                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">At SkyHarbour, we stand as a beacon of quality and authenticity in the world of spices, fruits, and vegetables. With an unwavering commitment to excellence, we meticulously source and deliver premium products that enrich culinary experiences. Based in India, Sky Harbour's journey is rooted in connecting global tables with the finest flavors from our farms.</p>
-                        <div class="flex mt-6 justify-center">
-                            <div class="w-16 h-1 rounded-full bg-green-500 inline-flex"></div>
-                        </div>
+                <div class="container px-5 py-24">
+                    <div class="text-center mb-5">
+                        <h1 className='title px-5'>Sky Harbour</h1>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s mt-3 para">At SkyHarbour, we stand as a beacon of quality and authenticity in the world of spices, fruits, and vegetables. With an unwavering commitment to excellence, we meticulously source and deliver premium products that enrich culinary experiences. Based in India, Sky Harbour's journey is rooted in connecting global tables with the finest flavors from our farms.</p>
                     </div>
-                    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                                </svg>
+                    <hr className='mb-5 w-50 mx-auto horiline' />
+                    <center>
+                        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6 abcard">
+                            <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                                <div className='abitem'>
+                                    <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-3 flex-shrink-0">
+                                        <img src={Farm} alt='farm' />
+                                    </div>
+                                    <div class="flex-grow">
+                                        <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Direct Farm Sourcing</h2>
+                                        <p class="leading-relaxed text-base">We take pride in sourcing our spices, fruits, and vegetables directly from farmers. This not only ensures the freshest produce but also empowers local communities.</p>
+
+                                    </div>
+                                </div>
                             </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Direct Farm Sourcing</h2>
-                                <p class="leading-relaxed text-base">We take pride in sourcing our spices, fruits, and vegetables directly from farmers. This not only ensures the freshest produce but also empowers local communities.</p>
-                                <a class="mt-3 text-green-500 inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
+                            <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                                <div className='abitem'>
+                                    <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-3 flex-shrink-0">
+                                        <img src={Quality} alt='Quality' />
+                                    </div>
+                                    <div class="flex-grow">
+                                        <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Quality Assurance</h2>
+                                        <p class="leading-relaxed text-base">Our commitment to quality surpasses industry standards. Rigorous quality control measures guarantee that every product we offer meets the highest international benchmarks.</p>
+
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <circle cx="6" cy="6" r="3"></circle>
-                                    <circle cx="6" cy="18" r="3"></circle>
-                                    <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                                </svg>
+
+                            <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                                <div className='abitem'>
+                                    <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-3 flex-shrink-0">
+                                        <img src={Handshake} alt='Handshake' />
+
+                                    </div>
+                                    <div class="flex-grow">
+                                        <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Reliability and Transparency</h2>
+                                        <p class="leading-relaxed text-base">Trust is paramount in international trade. Our commitment to reliability ensures a seamless export experience, allowing you to focus on your core business.</p>
+
+                                    </div>
+                                </div>
                             </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Quality Assurance</h2>
-                                <p class="leading-relaxed text-base">Our commitment to quality surpasses industry standards. Rigorous quality control measures guarantee that every product we offer meets the highest international benchmarks.</p>
-                                <a class="mt-3 text-green-500 inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                                    <circle cx="12" cy="7" r="4"></circle>
-                                </svg>
-                            </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-                                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
-                                <a class="mt-3 text-green-500 inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                                </svg>
-                            </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-                                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
-                                <a class="mt-3 text-green-500 inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
+                            <div class="p-4 md:w-1/2 flex flex-col text-center items-center">
+                                <div className='abitem'>
+                                    <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-3 flex-shrink-0">
+                                        <img src={Efficiency} alt='Efficiency' />
+                                    </div>
+                                    <div class="flex-grow">
+                                        <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Unlocking Efficiency</h2>
+                                        <p class="leading-relaxed text-base">Navigating the complexities of international markets can be both time-consuming and costly. By choosing SkyHarbour, you are tapping into our expertise, saving valuable resources.</p>
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <circle cx="6" cy="6" r="3"></circle>
-                                    <circle cx="6" cy="18" r="3"></circle>
-                                    <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-                                </svg>
-                            </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Reliability and Transparency</h2>
-                                <p class="leading-relaxed text-base">Trust is paramount in international trade. Our commitment to reliability ensures a seamless export experience, allowing you to focus on your core business.</p>
-                                <a class="mt-3 text-green-500 inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                                    <circle cx="12" cy="7" r="4"></circle>
-                                </svg>
-                            </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Unlocking Efficiency</h2>
-                                <p class="leading-relaxed text-base">Navigating the complexities of international markets can be both time-consuming and costly. By choosing SkyHarbour, you are tapping into our expertise, saving valuable resources.</p>
-                                <a class="mt-3 text-green-500 inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="flex mx-auto mt-16 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+                    </center>
                 </div>
             </section>
         </div>
